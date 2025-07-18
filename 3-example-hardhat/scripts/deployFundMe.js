4d61b6545dd59b5d7bb2569949ee5f36fb4c95e6
MakeCodingGreatAgain <makecodinggreatagain@gmail.com>
2025-07-18 17:32:59 +0800

    add docs

--- 3-example-hardhat/scripts/deployFundMe.js
+++ 3-example-hardhat/scripts/deployFundMe.js
@@ -12,25 +12,29 @@ async function main() {
     console.log(`contract has been deployed successfully, contract address is: ${ fundMe.target }`)
 
     const address = fundMe.target
-    console.log(`✅ Contract deployed at: ${address}`)
+    console.log(`✅ Contract deployed at: ${ address }`)
 
     // ✅ 验证是否部署到 sepolia
-    if(hre.network.config.chainId === 11155111 &&  process.env.ETHERSCAN_KEY) {
+    if (hre.network.config.chainId === 11155111 && process.env.ETHERSCAN_KEY) {
         // ✅ 等待 5 个区块确认再验证
         console.log("⏳ Waiting for 5 confirmations...")
         await fundMe.deploymentTransaction().wait(5)
 
         // ✅ 验证
-        await verifyFundMe(fundMe.target, [_lockTime])
+        await verifyFundMe(fundMe.target, [ _lockTime ])
+    }
+    else {
+        console.log("⚠️ Not on sepolia, skipping ")
     }
 }
 
 async function verifyFundMe(fundMeAddress, args) {
     await hre.run("verify:verify", {
         address: fundMeAddress,
-        constructorArguments:args
+        constructorArguments: args
     })
 }
+
 main().then(() => process.exit(0)).catch((error) => {
     console.error(error)
     process.exit(1)

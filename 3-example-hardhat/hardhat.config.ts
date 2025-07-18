57cc4691084dcb2038f3b1d9a821a74b59abf65d
MakeCodingGreatAgain <makecodinggreatagain@gmail.com>
2025-07-18 17:30:20 +0800

    verify:verify

--- 3-example-hardhat/hardhat.config.ts
+++ 3-example-hardhat/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config"
 import "@nomicfoundation/hardhat-toolbox"
+import "@nomicfoundation/hardhat-verify";
 // import * as dotenv from "dotenv"
 import * as dotenv from "@chainlink/env-enc"
 
@@ -7,7 +8,9 @@ dotenv.config()
 
 // QuickNode
 const SEPOLIA_RPC_URL_QUICKNODE = process.env.SEPOLIA_RPC_URL_QUICKNODE
-const SEPOLIA_KEY_QUICKNODE = process.env.SEPOLIA_KEY_QUICKNODE as string
+const SEPOLIA_KEY_QUICKNODE = process.env.SEPOLIA_KEY_QUICKNODE
+
+const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY
 
 const config: HardhatUserConfig = {
     solidity: "0.8.30",
@@ -17,9 +20,13 @@ const config: HardhatUserConfig = {
         hardhat: {},
         sepolia: {
             url: SEPOLIA_RPC_URL_QUICKNODE,
-            accounts: [ SEPOLIA_KEY_QUICKNODE ]
+            accounts: [ SEPOLIA_KEY_QUICKNODE as string ],
+            chainId: 11155111
         }
-    }
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_KEY
+    },
 }
 
 export default config

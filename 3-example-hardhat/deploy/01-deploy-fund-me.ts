098ced1d120606cbd138c7a2f59ffbe967d7a373
MakeCodingGreatAgain <makecodinggreatagain@gmail.com>
2025-07-21 20:05:14 +0800

    deploy fundme

--- 3-example-hardhat/deploy/01-deploy-fund-me.ts
+++ 3-example-hardhat/deploy/01-deploy-fund-me.ts
@@ -1,11 +1,44 @@
-const deployFundMe = async ({getNamedAccounts, deployments}) => {
+import { ethers } from "hardhat";
+import { DEVELOPMENT_CHAINS, LOCK_TIME, NETWORK_CONFIG } from "../config/helper-hardhat-config"
+
+const deployFundMe = async ({getNamedAccounts, deployments, network, run}) => {
     const {deployerAccount} = await getNamedAccounts()
-    await deployments.deploy("FundMe", {
+
+    // get dataFeedAddress
+    let dataFeedAddress
+
+    // local
+    if (DEVELOPMENT_CHAINS.includes(network.name)) {
+        const mockDataFeed = await deployments.get("MockV3Aggregator")
+        dataFeedAddress = mockDataFeed.address
+    }
+    // sepolia
+    else {
+        dataFeedAddress = NETWORK_CONFIG[network.config.chainId].ethUsdPriceFeed
+    }
+
+    // ✅ 验证是否部署到 sepolia
+    const isDeploySepolia = network.config.chainId === 11155111 && process.env.ETHERSCAN_KEY
+
+    // ✅ 部署合约
+    const args = [ LOCK_TIME, dataFeedAddress ]
+    const fundMe = await deployments.deploy("FundMe", {
         from: deployerAccount,
-        args: [ 180 ],
+        args,
         log: true,
-        waitConfirmations: 1
+        waitConfirmations: isDeploySepolia ? 5 : 0
     })
+
+    // ✅ 验证
+    if (isDeploySepolia) {
+        await run("verify:verify", {
+            address: fundMe.address,
+            constructorArguments: args
+        })
+    }
+    /*else {
+        console.log("⚠️ Not on sepolia, skipping ")
+    }*/
 }
 
 deployFundMe.tags = [ "all", "fundme" ]
